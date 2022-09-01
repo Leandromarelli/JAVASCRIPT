171cfd0ae33039e8ba478af77708a1be6e6b3ba8
Leandro Marelli <leandromarelli@gmail.com>
2022-09-01 20:34:52 -0300

    trabajo complementario 1

---
+++ main.js
@@ -0,0 +1,33 @@
+
+let nota = Number(prompt('ingrese su nota obtenida:'));
+
+if (nota >= 7) {
+    alert('Promociona la materia');
+} else if (nota<=6){
+    alert('No promociona la materia');
+} else {
+    alert ('ingrese un dato válido');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
